5121f3892611c68a5479266a7287c4102583341b
paithiov909 <am11.44@gmail.com>
2024-02-15 19:27:35 +0900

    cleanup

--- R/zzz.R
+++ R/zzz.R
@@ -3,8 +3,8 @@
     packageStartupMessage(
       paste(
         "You need 64-bit version of MeCab with UTF-8 dictionary on Windows.",
-        "Dowlnload it from 'https://github.com/ikegami-yukino/mecab/releases'",
-        "uninstall old 32-bit MeCab and install new 64 bit MeCab.",
+        "Download it from 'https://github.com/ikegami-yukino/mecab/releases',",
+        "uninstall old 32-bit MeCab and install new 64-bit MeCab.",
         sep = "\n"
       )
     )

b2759b84d23219965e4bc6c7d7b22c1212a64391
paithiov909 <am11.44@gmail.com>
2024-02-15 18:35:54 +0900

    fix packageStartupMessage

--- R/zzz.R
+++ R/zzz.R
@@ -1,21 +1,12 @@
-## .onLoad <- function(libname, pkgname)
-##   {
-
-## }
-
-## .onUnLoad <- function(libname, pkgname)
-## {
-
-## }
-
-#' @title .onAttach
-#' @description prints out a friendly reminder message to the user when
-#'              the RAQSAPI library is loaded.
-#' @inheritParams base .onAttech
-#' @return NULL
-#' @noRd
-.onAttach <- function(libname, pkgname)
-{
-    if(.Platform$OS.type == "windows")
-        packageStartupMessage("You need 64 bit version of MeCab with UTF8 dictionary on Windows.\nDowlnload it from 'https://github.com/ikegami-yukino/mecab/releases'\uninstall old 32 bit MeCab and install new 64 bit MeCab")
+.onAttach <- function(libname, pkgname) {
+  if (.Platform$OS.type == "windows") {
+    packageStartupMessage(
+      paste(
+        "You need 64-bit version of MeCab with UTF-8 dictionary on Windows.",
+        "Dowlnload it from 'https://github.com/ikegami-yukino/mecab/releases'",
+        "uninstall old 32-bit MeCab and install new 64 bit MeCab.",
+        sep = "\n"
+      )
+    )
   }
+}

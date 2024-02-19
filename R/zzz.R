540c190419142918a33e87459c2217d0c9334e7e
Ishida Motohiro <ishida.motohiro@tokushima-u.ac.jp>
2024-02-19 11:36:29 +0900

    Update zzz.R
    I made a correction because it could be misunderstood as if RMeCab was displaying the result of determining the version of MeCab at startup.

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,9 @@
   if (.Platform$OS.type == "windows") {
     packageStartupMessage(
       paste(
-        "You need 64-bit version of MeCab with UTF-8 dictionary on Windows.",
-        "Download it from 'https://github.com/ikegami-yukino/mecab/releases',",
-        "uninstall old 32-bit MeCab and install new 64-bit MeCab.",
+        "The current version is designed to work with the 64-bit version of MeCab and a UTF-8 dictionary. ",
+        "If you are using old MeCab with a Shift-JIS dictionary, please completely uninstall it, and",
+        "then reinstall the 64-bit version of MeCab with a UTF-8 dictionary from 'https://github.com/ikegami-yukino/mecab/releases'.",
         sep = "\n"
       )
     )
